30ac8916b192f8e450f357ef31d17502428689bc
Konrad Kuleta <konrad.kuleta26@gmail.com>
2024-05-03 11:00:59 +0200

    Creating new repo

---
+++ Sources/Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Konrad on 03.05.2024.
+//
+
+#include "Menu.h"
---
+++ Sources/Menu.h
@@ -0,0 +1,7 @@
+#ifndef PROJECT_AIZO_2_MENU_H
+#define PROJECT_AIZO_2_MENU_H
+
+
+
+
+#endif //PROJECT_AIZO_2_MENU_H
